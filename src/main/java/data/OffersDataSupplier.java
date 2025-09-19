73b309a4a304485d41e074c3a8b5ea2d7f9105be
giorgimjavia <giorgi.mjavia17@gmail.com>
2025-09-20 01:12:31 +0400

    Add Project

--- src/main/java/data/OffersDataSupplier.java
+++ src/main/java/data/OffersDataSupplier.java
@@ -6,11 +6,6 @@ public class OffersDataSupplier {
     @DataProvider(name = "offerFilters")
     public static Object[][] offersDataProvider() {
         return new Object[][]{
-//                {"Travel"},
-//                {"Shopping"},
-//                {"Cafe and Restaurant"},
-//                {"Home"},
-//                {"Developers"},
                 {"Category", "Travel"},
                 {"Product Type", "TBC Card"},
                 {"Offer Type", "Cashback"},
